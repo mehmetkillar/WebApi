075e0b9d6c72572dd90e4241d2ad129e958e53a3
mehmetkillar <49925113+mehmetkillar@users.noreply.github.com>
2022-01-21 23:01:49 +0300

    Add project files.

---
+++ WebApi/BookOperations/GetBookById/GetBookByIdQuery.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Common;
+using WebApi.DBOperations;
+
+namespace WebApi.BookOperations.GetBookById
+{
+    public class GetBookByIdQuery
+    {
+        private readonly BookStoreDbContext _dbContext;
+        public int BookId { get; set; }
+
+        public GetBookByIdQuery(BookStoreDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public BookByIdViewModel Handle()
+        {
+            var book = _dbContext.Books.Where(book => book.Id == BookId).SingleOrDefault();
+            if (book is null)
+                throw new InvalidOperationException("Kitap bulunamadı");
+            BookByIdViewModel vm = new BookByIdViewModel();
+            vm.Title = book.Title;
+            vm.PageCount = book.PageCount;
+            vm.PublishDate = book.PublishDate.Date.ToString("dd/MM/yyyy");
+            vm.Genre = ((GenreEnum)book.GenreId).ToString();
+            return vm;
+        }
+
+        public class BookByIdViewModel
+        {
+            public string Title { get; set; }
+            public string Genre { get; set; }
+            public int PageCount { get; set; }
+            public string PublishDate { get; set; }
+        }
+    }
+}
---
+++ WebApi/Controllers/BookController.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.BookOperations.CreateBook;
+using WebApi.BookOperations.DeleteBook;
+using WebApi.BookOperations.GetBookById;
+using WebApi.BookOperations.GetBooks;
+using WebApi.BookOperations.UpdateBook;
+using WebApi.DBOperations;
+using static WebApi.BookOperations.CreateBook.CreateBookCommand;
+using static WebApi.BookOperations.GetBookById.GetBookByIdQuery;
+using static WebApi.BookOperations.UpdateBook.UpdateBookCommand;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]s")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly BookStoreDbContext _context;
+        
+        public BookController(BookStoreDbContext context)
+        {
+            _context = context;
+        }
+        
+
+        [HttpGet]
+        public IActionResult GetBooks()
+        {
+            GetBooksQuery query = new GetBooksQuery(_context);
+            var result = query.Handle();
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            BookByIdViewModel result;
+            GetBookByIdQuery query = new GetBookByIdQuery(_context);
+            try
+            {
+                query.BookId = id;
+                result = query.Handle();
+
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+            return Ok(result);
+        }
+        //2 tane GET kabul etmiyor
+        //Queryden id alarak
+        //[HttpGet]
+        //public Book Get([FromQuery] string id)
+        //{
+        //    var book = BookList.Where(book => book.Id == Convert.ToInt32(id)).SingleOrDefault();
+        //    return book;
+        //}
+
+        [HttpPost]
+        public IActionResult AddBook([FromBody] CreateBookModel newBook)
+        {
+            CreateBookCommand command = new CreateBookCommand(_context);
+            try
+            {
+                command.Model = newBook;
+                command.Handle();
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateBook(string id,[FromBody] UpdateBookModel updatedBook)
+        {
+            UpdateBookCommand command = new UpdateBookCommand(_context);
+            try
+            {
+                command.BookId = Convert.ToInt32(id);
+                command.Model = updatedBook;
+                command.Handle();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBook(int id)
+        {
+            DeleteBookCommand command = new DeleteBookCommand(_context);
+            try
+            {
+                command.BookId = id;
+                command.Handle();
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+            return Ok();
+        }
+    }
+}
